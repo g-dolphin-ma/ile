b2e9578e5895016ee6001b2dc3aa270bfc827ae0
Stacey Ma <staceym2@illinois.edu>
2023-01-14 23:46:02 -0600

    Add NHAMCS dataset representation for PyTorch side

--- src/ile/dataset.py
+++ src/ile/dataset.py
@@ -2,22 +2,47 @@ import os
 import pandas as pd
 import torch
 import ile
+from torch.utils.data import Dataset
 
-class NHAMCSDataset(torch.Dataset):
+
+class NHAMCSDataset(Dataset):
     def __init__(self, year: int, transform=None, target_transform=None):
-        self.dataset = ile.io.NHAMCS(year)
+        self._dataset = ile.io.NHAMCS(year)
+        
+        self._x = self._generate_x()
+        self._y = self._generate_y()
+
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.dataset.data)
+        return len(self._y)
 
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+    def __getitem__(self, index):
+        x = self._x[index, :]
+        y = self._y[index]
         if self.transform:
-            image = self.transform(image)
+            features = self.transform(features)
         if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+            waittime = self.target_transform(waittime)
+        return x, y
+
+    def _generate_x(self):
+        columns = []
+        # RFV3D
+        columns += [f'RFV{i}3D' for i in range(1, 6)]
+        # RFV
+        columns += [f'RFV{i}' for i in range(1, 6)]
+
+        df = self._dataset.data[columns]
+        for column in columns:
+            # 1) create and concat new columns
+            df_dummies = pd.get_dummies(df[column], prefix=f'{column}_')
+            df = pd.concat([df, df_dummies], axis=1)
+            # 2) drop the original column
+            df.drop([column], axis=1, inplace=True)
+        
+        return df.to_numpy()
+
+    def _generate_y(self):
+        return self._dataset.data['WAITTIME'].values
